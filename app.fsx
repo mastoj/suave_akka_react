0ff18dd156dd7c2a76867e25a9a4de5f720ad781
Tomas Jansson <tomas.jansson@gmail.com>
2016-01-13 00:14:13 +0100

    maybe socket on server

--- app.fsx
+++ app.fsx
@@ -4,12 +4,14 @@
 #I "./packages/FSPowerPack.Linq.Community/lib/net40/"
 #I "./packages/Akka.FSharp/lib/net45/"
 #I "./packages/Suave/lib/net40/"
+#I "./packages/FSharp.Data/lib/net40"
 
 #r "Suave.dll"
 #r "Akka.dll"
 #r "FsPickler.dll"
 #r "FSharp.PowerPack.Linq.dll"
 #r "Akka.FSharp.dll"
+#r "FSharp.Data"
 
 #load "chat.fsx"
 
@@ -21,6 +23,8 @@ open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 open Suave.RequestErrors
+open Suave.Sockets
+open Suave.WebSocket
 open System.IO
 open System.Threading
 open Chat
@@ -50,12 +54,61 @@ let serveContent (contentTypeStr,str) =
     >=> OK (file (relPath + str))
 
 module API = 
+    open FSharp.Data
+    type JsonTypes = JsonProvider<"""
+    {
+        "Users": [{"UserName": "John doe"}],
+        "Notification": {
+            "Message": "This is the message",
+            "UserName": "John Doe"
+        } 
+    }
+    """>
+
+    
+
+    // let connect roomName :WebPart=
+    //     let response = JsonValue.Array(["tomas"; "Stuart"] |> List.map (fun n -> JsonTypes.User(n).JsonValue) |> List.toArray) 
+    //     Writers.setMimeType "application/json"
+    //     >=> OK (response.ToString())
+
+    let getUsers roomName = 
+        let response = JsonValue.Array(["tomas"; "Stuart"] |> List.map (fun n -> JsonTypes.User(n).JsonValue) |> List.toArray) 
+        Writers.setMimeType "application/json"
+        >=> OK (response.ToString())
+
+    open Suave.Sockets.Control
+    open Suave.Sockets.SocketOp
+    let utf8Bytes (str:string) = System.Text.Encoding.UTF8.GetBytes(str)
+    let utf8String (bytes:byte []) = System.Text.Encoding.UTF8.GetString(bytes)
+    
+    let joinRoom (chat:Chat.Chat) roomName userName (webSocket : WebSocket) cx = 
+        let notificationFunc (Chat.Message msg, Chat.UserName name) = 
+            let response = JsonTypes.Notification(msg, name).JsonValue.ToString()
+            async {
+                let! x = webSocket.send Opcode.Text (utf8Bytes response) true
+                match x with | _ -> return ()
+            } |> Async.RunSynchronously
+            
+        let sendMsgFunc = chat.JoinRoom (Chat.RoomName roomName) (Chat.UserName userName) notificationFunc
+
+        socket {
+            while true do
+                let! inChoice = webSocket.read()
+                match inChoice with
+                | (Opcode.Text, bytes, true) ->
+                    let msgStr = utf8String bytes 
+                    sendMsgFunc (Chat.Message msgStr)
+                | _ -> ()
+        }
+
     let app (chat:Chat.Chat) = 
         choose 
             [
                 POST >=> pathScan "/api/room/%s" (fun name -> chat.CreateRoom (Chat.RoomName name); OK ("Created " + name))
-                path "/api/connect" >=> 
-                    GET >=> OK "Hello"
+//                POST >=> pathScan "/api/room/%s/join" connect
+                GET >=> pathScan "/api/room/%s/users" getUsers
+                pathScan "/api/room/%s/join/%s" (fun (roomName,userName) -> handShake (joinRoom chat roomName userName))
             ]
 
 let content = 
@@ -76,6 +129,6 @@ let app() =
 let _, server = startWebServerAsync config (app())
 Async.Start(server, token)
 printfn "Started"
-//cancellationTokenSource.Cancel()
+cancellationTokenSource.Cancel()
 
 System.Console.ReadLine()
